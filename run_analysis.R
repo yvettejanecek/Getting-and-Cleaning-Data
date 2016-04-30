1da6d64baa090e4aa99989ad67934dca0891eb4f
yvettejanecek <yvette.janecek@gmail.com>
2016-04-30 14:31:59 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(tidyr)
 library(data.table)
 
-#download and unzip data set
+#Download and unzip data set
 if(!file.exists("./SamsungGalaxS.zip")){
 	url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 	download.file(url, "SamsungGalaxyS.zip")
@@ -23,19 +23,34 @@ features <- read.table("./SamsungGalaxyS/UCI HAR Dataset/features.txt")
 
 #Merge test and traing data and add descriptive names
 combine_data_subject <- rbind(subject_train, subject_test)
-colnames(combine_data_subject) <- "subjects_number"  
+colnames(combine_data_subject) <- "SubjectNumber"  
 combine_data_x <- rbind(x_train, x_test)
 colnames(combine_data_x) <- features$V2  #add feature names
 combine_data_y <- rbind(y_train, y_test)
 combine_data_y_activity <- merge(combine_data_y, activity, by="V1", sort=FALSE) #add activity names to data
-colnames(combine_data_y_activity) <- c("activity_number", "ACTIVITY")
+colnames(combine_data_y_activity) <- c("ActivityNumber", "ACTIVITY")
 combine_data <- cbind(combine_data_subject, combine_data_y_activity, combine_data_x)
 
 #Extract mean and standard deviation for each measurement
 mean_std_rows <- grep("mean()|std()", features$V2) #determine column numbers with mean or std
 extracted_data <- combine_data[mean_std_rows, ]
 
+#Clean names
+names(extracted_data) <- gsub("Acc", "Acelerometer", names(extracted_data))
+names(extracted_data) <- gsub("Gyro", "Gyroscope", names(extracted_data))
+names(extracted_data) <- gsub("BodyBody", "Body", names(extracted_data))
+names(extracted_data) <- gsub("Mag", "Magnitude", names(extracted_data))
+names(extracted_data) <- gsub("^t", "Time", names(extracted_data))
+names(extracted_data) <- gsub("^f", "Frequency", names(extracted_data))
+names(extracted_data) <- gsub("tBody", "TimeBody", names(extracted_data))
+names(extracted_data) <- gsub("-mean()", "Mean", names(extracted_data))
+names(extracted_data) <- gsub("-std()", "StandardDeviation", names(extracted_data))
+names(extracted_data) <- gsub("-freq()", "Frequencey", names(extracted_data))
+names(extracted_data) <- gsub("angle", "Angle", names(extracted_data))
+names(extracted_data) <- gsub("gravity", "Gravity", names(extracted_data))
+names(extracted_data) <- gsub("()", "", names(extracted_data), fixed=TRUE)
+
 #Create tidy data set with mean of each activity and each subject
- tidy_data <- data.table(extracted_data)[ , lapply(.SD, mean), by="subjects_number,activity_number"]
+tidy_data <- data.table(extracted_data)[ , lapply(.SD, mean), by="SubjectNumber,ActivityNumber"]
 
 write.table(tidy_data, file="TidyRunAnalysis.txt", row.name=FALSE)
